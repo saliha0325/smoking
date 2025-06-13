78a275d34fa46bee757a0f3939b8fc9212c0d1d6
saliha0325 <162295569+saliha0325@users.noreply.github.com>
2025-06-14 00:01:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var smokemachine = function (context, color){
-    color = color || [110, 79, 38]; // Darker brown color
+    color = color || [24, 46.8, 48.2];
     var polyfillAnimFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame ||
                                   window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
     var lastframe;
@@ -12,81 +12,73 @@ var smokemachine = function (context, color){
     buffer.width = 20;
     buffer.height = 20;
 
-    // Increased opacities (scaled up for better visibility)
-    var opacities = [
-        // Scaled-up version of the original opacities
-        // Many zeros removed for better effect visibility
-        ...Array(50).fill(0), ...Array(50).fill(20),
-        ...Array(50).fill(50), ...Array(50).fill(80),
-        ...Array(50).fill(120), ...Array(50).fill(160),
-        ...Array(50).fill(200), ...Array(50).fill(255)
-    ];
+    var opacities = [0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,3,5,5,7,4,4,1,1,0,1,0,0,0,0,0,1,0,0,17,27,41,52,56,34,23,15,11,4,9,5,1,0,0,0,0,0,0,1,45,63,57,45,78,66,52,41,34,37,23,20,0,1,0,0,0,0,1,43,62,66,64,67,115,112,114,56,58,47,33,18,12,10,0,0,0,0,39,50,63,76,87,107,105,112,128,104,69,64,29,18,21,15,0,0,0,7,42,52,85,91,103,126,153,128,124,82,57,52,52,24,1,0,0,0,2,17,41,67,84,100,122,136,159,127,78,69,60,50,47,25,7,1,0,0,0,34,33,66,82,113,138,149,168,175,82,142,133,70,62,41,25,6,0,0,0,18,39,55,113,111,137,141,139,141,128,102,130,90,96,65,37,0,0,0,2,15,27,71,104,129,129,158,140,154,146,150,131,92,100,67,26,3,0,0,0,0,46,73,104,124,145,135,122,107,120,122,101,98,96,35,38,7,2,0,0,0,50,58,91,124,127,139,118,121,177,156,88,90,88,28,43,3,0,0,0,0,30,62,68,91,83,117,89,139,139,99,105,77,32,1,1,0,0,0,0,0,16,21,8,45,101,125,118,87,110,86,64,39,0,0,0,0,0,0,0,0,0,1,28,79,79,117,122,88,84,54,46,11,0,0,0,0,0,0,0,0,0,1,0,6,55,61,68,71,30,16,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,14,23,25,20,12,5,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,4,2,2,0,0,0,0,0,0,0,0];
 
-    var data = bctx.createImageData(20, 20);
+    var data = bctx.createImageData(20,20);
     var d = data.data;
 
-    for (var i = 0; i < d.length; i += 4) {
-        d[i] = color[0];
-        d[i + 1] = color[1];
-        d[i + 2] = color[2];
-        d[i + 3] = opacities[i / 4] || 0;
+    for(var i=0;i<d.length;i+=4){
+        d[i]=color[0];
+        d[i+1]=color[1];
+        d[i+2]=color[2];
+        d[i+3]=opacities[i / 4];
     }
 
-    bctx.putImageData(data, 0, 0);
+    bctx.putImageData(data,0,0);
 
     var imagewidth = 20 * 5;
     var imageheight = 20 * 5;
 
-    function particle(x, y, l) {
+    function particle(x,y,l){
         this.x = x;
         this.y = y;
         this.age = 0;
-        this.vx = (Math.random() * 8 - 4) / 100;
-        this.startvy = -(Math.random() * 30 + 10) / 100;
+        this.vx = (Math.random()*8-4)/100;
+        this.startvy = -(Math.random()*30+10)/100;
         this.vy = this.startvy;
-        this.scale = Math.random() * .5;
-        this.lifetime = Math.random() * l + l / 2;
-        this.finalscale = 5 + this.scale + Math.random();
+        this.scale = Math.random()*.5;
+        this.lifetime = Math.random()*l+l/2;
+        this.finalscale = 5+this.scale+Math.random();
 
-        this.update = function (deltatime) {
-            this.x += this.vx * deltatime;
-            this.y += this.vy * deltatime;
-            var frac = Math.pow((this.age) / this.lifetime, .5);
-            this.vy = (1 - frac) * this.startvy;
-            this.age += deltatime;
-            this.scale = frac * this.finalscale;
+        this.update = function(deltatime){
+            this.x+=this.vx*deltatime;
+            this.y+=this.vy*deltatime;
+            var frac = Math.pow((this.age)/this.lifetime,.5);
+            this.vy = (1-frac)*this.startvy;
+            this.age+=deltatime;
+            this.scale=frac*this.finalscale;
         };
 
-        this.draw = function () {
-            context.globalAlpha = (1 - Math.abs(1 - 2 * (this.age) / this.lifetime)) / 3;
-            var off = this.scale * imagewidth / 2;
-            var xmin = this.x - off;
-            var xmax = xmin + this.scale * imageheight;
-            var ymin = this.y - off;
-            var ymax = ymin + this.scale * imageheight;
-            context.drawImage(buffer, xmin, ymin, xmax - xmin, ymax - ymin);
+        this.draw = function(){
+            context.globalAlpha = (1-Math.abs(1-2*(this.age)/this.lifetime))/8;
+            var off = this.scale*imagewidth/2;
+            var xmin = this.x-off;
+            var xmax = xmin+this.scale*imageheight;
+            var ymin = this.y-off;
+            var ymax = ymin+this.scale*imageheight;
+            context.drawImage(buffer, xmin, ymin, xmax-xmin, ymax-ymin);
         };
     }
 
-    function addparticles(x, y, n, lifetime) {
+    function addparticles(x,y,n,lifetime){
         lifetime = lifetime || 4000;
         n = n || 10;
-        if (n < 1) return Math.random() <= n && pendingparticles.push(new particle(x, y, lifetime));
+        if(n < 1) return Math.random() <= n && pendingparticles.push(new particle(x,y,lifetime));
         for (var i = 0; i < n; i++) {
-            pendingparticles.push(new particle(x, y, lifetime));
+            pendingparticles.push(new particle(x,y,lifetime));
         };
     }
 
-    function updateanddrawparticles(deltatime) {
+    function updateanddrawparticles(deltatime){
         context.clearRect(0, 0, canvas.width, canvas.height);
         deltatime = deltatime || 16;
         var newparticles = [];
         currentparticles = currentparticles.concat(pendingparticles);
         pendingparticles = [];
 
-        currentparticles.forEach(function (p) {
+        currentparticles.forEach(function(p){
             p.update(deltatime);
-            if (p.age < p.lifetime) {
+            if (p.age<p.lifetime){
                 p.draw();
                 newparticles.push(p);
             }
@@ -94,57 +86,62 @@ var smokemachine = function (context, color){
         currentparticles = newparticles;
     }
 
-    function frame(time) {
-        if (running) {
-            var deltat = time - lastframe;
+    function frame(time){
+        if(running){
+            var deltat = time-lastframe;
             lastframe = time;
+
             updateanddrawparticles(deltat);
             polyfillAnimFrame(frame);
         }
     }
 
     var running = false;
-    function start() {
+    function start(){
         running = true;
-        polyfillAnimFrame(function (time) {
+        polyfillAnimFrame(function(time){
             lastframe = time;
             polyfillAnimFrame(frame);
         });
     }
 
-    function stop() {
+    function stop(){
         running = false;
     }
 
     return {
-        start: start,
-        stop: stop,
+        start:start,
+        stop:stop,
         step: updateanddrawparticles,
         addsmoke: addparticles
     }
 }
 
-// Setup canvas and start smoke
 var canvas = document.getElementById('canvas');
-var ctx = canvas.getContext('2d');
-canvas.width = innerWidth;
-canvas.height = innerHeight;
+var context = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
-// Dark brown color: #6e4f26
-var party = smokemachine(ctx, [110, 79, 38]); 
+// Dark brown RGB color
+var darkBrown = [101, 67, 33];  // Dark brown tone
+
+var party = smokemachine(context, darkBrown);
 party.start();
 
-// Mouse interaction
-onmousemove = function (e) {
+window.onmousemove = function (e) {
     var x = e.clientX;
     var y = e.clientY;
-    var n = .5;
+    var n = 0.5;
     var t = Math.floor(Math.random() * 200) + 3800;
     party.addsmoke(x, y, n, t);
 };
 
-// Constant smoke from bottom
-setInterval(function () {
-    const randomX = Math.random() * innerWidth;
-    party.addsmoke(randomX, innerHeight, 1);
+setInterval(function(){
+    var randomX = Math.random() * window.innerWidth;
+    party.addsmoke(randomX, window.innerHeight, 1);
 }, 100);
+
+window.onresize = function(){
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+};
