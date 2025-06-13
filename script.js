017942947a275c7ff10a958120955f398e5b78df
saliha0325 <162295569+saliha0325@users.noreply.github.com>
2025-06-14 00:39:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-var smokemachine = function (context, color){
-    // color is ignored here since we randomize inside particle creation now
+var smokemachine = function (context){
+    // Mix of pure white and light brown (average)
+    var color = [232, 217, 197];  // mixed color of white and light brown
 
     var polyfillAnimFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame ||
                                   window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
@@ -15,18 +16,17 @@ var smokemachine = function (context, color){
 
     var opacities = [0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,3,5,5,7,4,4,1,1,0,1,0,0,0,0,0,1,0,0,17,27,41,52,56,34,23,15,11,4,9,5,1,0,0,0,0,0,0,1,45,63,57,45,78,66,52,41,34,37,23,20,0,1,0,0,0,0,1,43,62,66,64,67,115,112,114,56,58,47,33,18,12,10,0,0,0,0,39,50,63,76,87,107,105,112,128,104,69,64,29,18,21,15,0,0,0,7,42,52,85,91,103,126,153,128,124,82,57,52,52,24,1,0,0,0,2,17,41,67,84,100,122,136,159,127,78,69,60,50,47,25,7,1,0,0,0,34,33,66,82,113,138,149,168,175,82,142,133,70,62,41,25,6,0,0,0,18,39,55,113,111,137,141,139,141,128,102,130,90,96,65,37,0,0,0,2,15,27,71,104,129,129,158,140,154,146,150,131,92,100,67,26,3,0,0,0,0,46,73,104,124,145,135,122,107,120,122,101,98,96,35,38,7,2,0,0,0,50,58,91,124,127,139,118,121,177,156,88,90,88,28,43,3,0,0,0,0,30,62,68,91,83,117,89,139,139,99,105,77,32,1,1,0,0,0,0,0,16,21,8,45,101,125,118,87,110,86,64,39,0,0,0,0,0,0,0,0,0,1,28,79,79,117,122,88,84,54,46,11,0,0,0,0,0,0,0,0,0,1,0,6,55,61,68,71,30,16,0,0,0,0,0,0,0,0,0,0,0,0,0,0,14,23,25,20,12,5,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,12,9,8,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,4,2,2,0,0,0,0,0,0,0,0];
 
-    // We'll create a function to set pixel data in the buffer for given color
-    function createSmokeTexture(color) {
-        var data = bctx.createImageData(20,20);
-        var d = data.data;
-        for(var i=0; i<d.length; i+=4){
-            d[i] = color[0];
-            d[i+1] = color[1];
-            d[i+2] = color[2];
-            d[i+3] = opacities[i / 4];
-        }
-        bctx.putImageData(data,0,0);
+    var data = bctx.createImageData(20,20);
+    var d = data.data;
+
+    for(var i=0; i<d.length; i+=4){
+        d[i] = color[0];
+        d[i+1] = color[1];
+        d[i+2] = color[2];
+        d[i+3] = opacities[i / 4];
     }
+
+    bctx.putImageData(data, 0, 0);
 
     var imagewidth = 20 * 5;
     var imageheight = 20 * 5;
@@ -41,16 +41,6 @@ var smokemachine = function (context, color){
         this.scale = Math.random()*.5;
         this.lifetime = Math.random()*l+l/2;
         this.finalscale = 5+this.scale+Math.random();
-
-        // Randomly choose white or light brown for each particle's texture:
-        var smokeColors = [
-            [255, 255, 255],   // pure white
-            [210, 180, 140]    // light brown (tan)
-        ];
-        this.colorChoice = smokeColors[Math.floor(Math.random() * smokeColors.length)];
-
-        // Create the smoke texture for this particle's color in the buffer
-        createSmokeTexture(this.colorChoice);
 
         this.update = function(deltatime){
             this.x += this.vx * deltatime;
@@ -82,7 +72,7 @@ var smokemachine = function (context, color){
     }
 
     function updateanddrawparticles(deltatime){
-        context.clearRect(0, 0, canvas.width, canvas.height);   
+        context.clearRect(0, 0, canvas.width, canvas.height);
         deltatime = deltatime || 16;
         var newparticles = [];
         currentparticles = currentparticles.concat(pendingparticles);
@@ -104,7 +94,7 @@ var smokemachine = function (context, color){
             lastframe = time;
 
             updateanddrawparticles(deltat);
-            polyfillAnimFrame(frame);            
+            polyfillAnimFrame(frame);
         }
     }
 
@@ -114,7 +104,7 @@ var smokemachine = function (context, color){
         polyfillAnimFrame(function(time){
             lastframe = time;
             polyfillAnimFrame(frame);
-        });          
+        });
     }
 
     function stop(){
@@ -122,8 +112,8 @@ var smokemachine = function (context, color){
     }
 
     return {
-        start:start,
-        stop:stop,
+        start: start,
+        stop: stop,
         step: updateanddrawparticles,
         addsmoke: addparticles
     }
@@ -134,7 +124,7 @@ var ctx = canvas.getContext('2d');
 canvas.width = innerWidth;
 canvas.height = innerHeight;
 
-var party = smokemachine(ctx);  // color arg is ignored now
+var party = smokemachine(ctx);
 party.start();
 
 onmousemove = function (e) {
