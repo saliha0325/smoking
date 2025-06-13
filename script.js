35cb53470a47557298a2e536999c6cbd37a3f156
saliha0325 <162295569+saliha0325@users.noreply.github.com>
2025-06-13 22:19:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,99 @@
 var smokemachine = function (context) {
-    var polyfillAnimFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame ||
-        window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
+  var raf = window.requestAnimationFrame || window.mozRequestAnimationFrame ||
+            window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
+  var lastframe, currentparticles = [], pendingparticles = [];
 
-    var lastframe;
-    var currentparticles = [];
-    var pendingparticles = [];
+  function createBuffer(color) {
+    var buffer = document.createElement('canvas'),
+        bctx = buffer.getContext('2d');
+    buffer.width = buffer.height = 20;
+    var opacities = [/* same opacity array as before */];
 
-    function createBuffer(color) {
-        var buffer = document.createElement('canvas'),
-            bctx = buffer.getContext('2d');
+    var data = bctx.createImageData(20, 20);
+    for (var i = 0; i < data.data.length; i += 4) {
+      data.data[i] = color[0];
+      data.data[i+1] = color[1];
+      data.data[i+2] = color[2];
+      data.data[i+3] = opacities[i/4];
+    }
+    bctx.putImageData(data, 0, 0);
+    return buffer;
+  }
 
-        buffer.width = 20;
-        buffer.height = 20;
+  var buffers = [
+    createBuffer([255, 150, 150]), // light red
+    createBuffer([255, 75, 75]),   // medium red
+    createBuffer([255, 0, 0]),     // pure red
+  ];
 
-        var opacities = [
-            0, 0, 1, 1, 2, 2, 4, 4, 6, 6, 9, 9, 13, 13, 18, 18, 24, 24, 31, 31,
-            39, 39, 48, 48, 58, 58, 69, 69, 81, 81, 94, 94, 108, 108, 123, 123,
-            139, 139, 156, 156, 174, 174, 193, 193, 213, 213, 234, 234, 255, 255,
-            234, 234, 213, 213, 193, 193, 174, 174, 156, 156, 139, 139, 123, 123,
-            108, 108, 94, 94, 81, 81, 69, 69, 58, 58, 48, 48,
+  function particle(x, y, l) {
+    var buf = buffers[Math.floor(Math.random()*buffers.length)];
+    this.buffer = buf;
+    this.x = x; this.y = y; this.age = 0;
+    this.vx = (Math.random()*8-4)/100;
+    this.startvy = -(Math.random()*30+10)/100;
+    this.vy = this.startvy;
+    this.lifetime = Math.random()*l + l/2;
+    this.finalscale = 10 + Math.random()*3;
+  }
+
+  particle.prototype.update = function(dt) {
+    this.x += this.vx*dt;
+    this.y += this.vy*dt;
+    var frac = Math.sqrt(this.age/this.lifetime);
+    this.vy = this.startvy*(1-frac);
+    this.age += dt;
+    this.scale = frac*this.finalscale;
+  };
+
+  particle.prototype.draw = function() {
+    var alpha = (1 - Math.abs(1 - 2*this.age/this.lifetime)) / 8;
+    context.globalAlpha = alpha;
+    var size = this.scale * 100; // canvas scaling
+    context.drawImage(this.buffer, this.x-size/2, this.y-size/2, size, size);
+  };
+
+  function addparticles(x, y, n, l) {
+    n = n || 10;
+    for (var i = 0; i < n; i++) {
+      pendingparticles.push(new particle(x, y, l || 4000));
+    }
+  }
+
+  function update(dt) {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    var np = [];
+    currentparticles = currentparticles.concat(pendingparticles);
+    pendingparticles = [];
+    currentparticles.forEach(p => {
+      p.update(dt);
+      if (p.age < p.lifetime) {
+        p.draw();
+        np.push(p);
+      }
+    });
+    currentparticles = np;
+  }
+
+  function frame(time) {
+    if (!lastframe) lastframe = time;
+    update(time - lastframe);
+    lastframe = time;
+    raf(frame);
+  }
+
+  return {
+    start: () => raf(frame),
+    addsmoke: addparticles
+  };
+};
+
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+canvas.width = innerWidth; canvas.height = innerHeight;
+
+var party = smokemachine(ctx);
+party.start();
+
+onmousemove = (e) => party.addsmoke(e.clientX, e.clientY, 5);
+setInterval(() => party.addsmoke(canvas.width/2, canvas.height, 7), 100);
