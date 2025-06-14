4344288f7769ce8b286ad0292759a3d88d98ca78
saliha0325 <162295569+saliha0325@users.noreply.github.com>
2025-06-14 13:30:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var smokemachine = function (context){
     // Mix of pure white and light brown (average)
-    var color = [232, 217, 197];  // mixed color of white and light brown
+    var color = [180, 165, 145];  // mixed color of white and light brown
 
     var polyfillAnimFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame ||
                                   window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
